49ccc8e047989d8d529786f8ce46db4d97b41643
RenatoFringuello <renatofringuello@hotmail.it>
2022-12-23 15:17:38 +0100

    add AppHero component as jumbotron

---
+++ vite-hello-world/src/components/AppHero.vue
@@ -0,0 +1,31 @@
+<script>
+import AppTitle from './AppTitle.vue';
+
+export default {
+    components:{
+        AppTitle
+    }
+}
+</script>
+
+<template>
+    
+    <div>
+        <AppTitle />
+    </div>
+
+</template>
+
+<style scoped>
+    div{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: white;
+        width: 100%;
+        height:400px;
+        background:url('../assets/space.webp');
+        background-position: center;
+        background-size:cover;
+    }
+</style>
